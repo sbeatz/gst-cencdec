06e7cff9bee37503ea77644f64216a55714df52a
sbeatz <sbeatz76@googlemail.com>
2016-10-06 08:15:15 +0200

    Create cdm_adapter_interface.cpp

---
+++ src/cdm/media/cdm/cdm_adapter_interface.cpp
@@ -0,0 +1,28 @@
+#include "cdm_adapter_interface.h"
+#include "cdm_adapter.h"
+
+extern "C" 
+{
+
+	wvadapter * wvadapter_init()
+	{
+		media::CdmAdapter *adapter = new media::CdmAdapter("com.widevine.alpha", "/usr/lib/libwindevinecdm.so", "", media::CdmConfig(false, true),*(dynamic_cast<media::CdmAdapterClient*>(this)));
+		return (wvadapter *)adapter;
+	}
+	
+	void wvadapter_CreateSessionAndGenerateRequest(const wvadapter *adapter, const uint8_t* init_data,
+		uint32_t init_data_size)
+	{
+		media::CdmAdapter *adapter = (wvadapter *)adapter;
+		adapter->CreateSessionAndGenerateRequest(0, cdm::SessionType::kTemporary, cdm::InitDataType::kCenc, init_data, init_data_size);
+	}
+	
+	bool wvadapter_valid(const wvadapter *adapter)
+	{
+		media::CdmAdapter *adapter = (wvadapter *)adapter;
+		return adapter->valid();
+	}
+
+	
+
+}
